c4e3d30e91a23230330a8a4f328ff7656edc0bca
vra <wyf.brz@gmail.com>
2023-09-24 21:47:23 +0800

    test: udpate test result after test img resize

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -5,4 +5,4 @@ def test_sdk(load_test_img, load_sdk_default):
     img = load_test_img
     sdk = load_sdk_default
     result = sdk.process(img)
-    assert np.allclose(int(result.mean()), 205)
+    assert np.allclose(int(result.mean()), 203)
